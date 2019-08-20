d8aee4d38e08df634a6cbce8220f5216f687cc03
Thomas Bollmeier <developer@thomas-bollmeier.de>
2019-08-20 22:46:39 +0200

    absoluteValuesSumMinimization 1st attempt

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,22 @@
 function absoluteValuesSumMinimization(a: number[]): number {
 
+    const half = Math.ceil(a.reduce((s, i) => s + i, 0) / 2);
+    
+    let sum = 0;
+    let median = null;
+
+    for (let value of a) {
+        if (sum + value <= half) {
+            sum += value;
+            median = value;
+        } else {
+            break;
+        }
+    }
+
+    return median;    
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+//console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
